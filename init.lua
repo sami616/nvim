060707d07913d5b3befce238b85658d4c34cda69
sami616 <samuelresua@googlemail.com>
2023-07-25 13:34:57 +0100

    wip

--- init.lua
+++ init.lua
@@ -183,7 +183,8 @@ require('lazy').setup({
             vim.keymap.set('n', '<leader>fl', require('telescope.builtin').resume, { desc = '[F]ind [L]last' })
             vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind [W]ord' })
             vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind [G]rep' })
-            vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[R]ind [D]iagnostics' })
+            vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
+            vim.keymap.set('n', '<leader>fk', require('telescope.builtin').keymaps, { desc = '[F]ind [K]eymaps' })
             vim.keymap.set('n', '<leader>fr', require('telescope.builtin').lsp_references,
                 { desc = '[F]ind [R]eferences' })
             vim.keymap.set('n', '<leader>fs', require('telescope.builtin').lsp_document_symbols,
