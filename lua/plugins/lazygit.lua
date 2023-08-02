bd877b184756051c6b061b427d36327f6630cf2f
sami616 <samuelresua@googlemail.com>
2023-08-02 09:28:50 +0100

    wip

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,7 +1,7 @@
 return {
     'kdheepak/lazygit.nvim',
-    keys = { { '<leader>g', desc = 'Git', mode = 'n' } },
+    keys = { { '<leader>sc', desc = 'Git', mode = 'n' } },
     config = function()
-        vim.keymap.set('n', '<leader>g', ':LazyGit<CR>', { desc = 'Git' })
+        vim.keymap.set('n', '<leader>sc', ':LazyGit<CR>', { desc = '[S]ource [C]ontrol' })
     end,
 }
