f2aca5eae8b2b47bee145944423fc6bf9d81dcb5
sami616 <samuel.resua@bp.com>
2023-12-06 12:00:22 +0000

    wip

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ require('packages')
 require('keymaps')
 
 -- Highlight Yanks
-
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
 	callback = function()
